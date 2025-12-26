b57402c9d0e78d6e58c35458d1a6a50daaeda92d
rickimacri <rickimacri@gmail.com>
2025-12-26 01:40:23 +0100

    Delete script.js
    nn uso

--- script.js
+++
@@ -1,5 +0,0 @@
-// Menu mobile toggle (facoltativo)
-const menu = document.querySelector('nav');
-document.querySelector('.logo img').addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
